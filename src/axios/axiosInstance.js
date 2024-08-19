b668a0c5923915bda666358c45a882c03734f7c1
Umair Khan <uk125896@gmail.com>
2024-08-19 15:39:11 +0530

    Committing after makign list of savedItems

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -11,11 +11,11 @@ const axiosInstance = axios.create({
 });
 
 
-
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = Cookies.get('access_token');
+    console.log("token is ",token)
     if (token!=='null' && token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
